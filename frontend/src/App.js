3fa731597e0e2874f91d5c4ceefc23e949dc7917
fillipe <fillipe.leao.cefet@gmail.com>
2020-03-26 18:50:23 -0300

    Frontend do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './global.css';
+// jsx (javascript e XML)
+// componente é uma função que retorna html
+import Routes from './routes';
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>PANDA CAPAS</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Routes/>    
     );
-  }
 }
 
 export default App;
